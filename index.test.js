71e66a6e993d969a8c6dad97f11b199423cf10bc
Deadpool1 <atwood169@yahoo.com>
2023-05-28 22:01:37 -0500

    hard coded to check test

--- index.test.js
+++ index.test.js
@@ -11,3 +11,11 @@ test('capitalize first character in string', () => {
 test('function takes string and returns it reversed', () => {
     expect(reverseString('')).toBe('')
 })
+
+// Calculator
+test('add, subtract, divide and mulitply two numbers', () => {
+    expect(2 + 2).toBe(4),
+    expect(2 - 2).toBe(0),
+    expect(2 / 2).toBe(1),
+    expect(2 * 2).toBe(4)
+})
